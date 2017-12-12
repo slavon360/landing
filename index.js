6f9ee69c042cd79e3cfa48aa4c8971b2a3c060f0
unknown <slavon360@gmail.com>
2017-12-12 14:08:26 +0200

    port has been changed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(5000, function(){
-  console.log('server is running ...')
-})
+app.set('port', (process.env.PORT || 80));
+
+app.listen(app.get('port'), function(){
+  console.log('server is running ...');
+});
 
 app.use(express.static('public'));
